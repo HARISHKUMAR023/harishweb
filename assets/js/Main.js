31a190c7282aba82bcd81b1523b52ab7994ad44a
HARISHKUMAR N <harishkumarsp023@gmail.com>
2024-10-03 17:00:46 +0530

    add some ui

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -1,3 +1,114 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS('particles-js',{
+    "particles": {
+      "number": {
+        "value": 400,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#fff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 10,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 500,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 2
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "bottom",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 0.5
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 4,
+          "duration": 0.3,
+          "opacity": 1,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  })
 //  typing Animation config code 
 
  new TypeIt("#simpleUsage", {
@@ -6,8 +117,12 @@
     waitUntilVisible: true,
 }).go();
 
-
-
+// for cursor
+new kursor({
+    removeDefaultCursor: true,
+    type: 1,
+     color: '#ea580c'
+})
 const carddata = [
     {
         icone:"./assets/img/icone/html-5.png",
@@ -104,7 +219,7 @@ $(document).ready(function(){
 //     }
     carddata.map((data,index)=>{
    $("#skills").append(`
-    <div class="bg-[#66686c36] p-10 flex justify-items-center item-center w-32 h-34 rounded-md hover:bg-orange-400 m-4">
+    <div class="bg-[#66686c36] p-10 flex justify-items-center item-center w-32 h-34 rounded-md hover:bg-orange-600 m-4">
     <img src=${data.icone} class="w-16 h-16">
 
     </div>`)
